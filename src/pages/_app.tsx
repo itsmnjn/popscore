ff9e6693bdf2068690bff8507be97f31898ee60c
Min Jun Kim <eric@itsmnjn.com>
2022-09-16 03:18:06 -0400

    feat: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 
 import '@app/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string);
+    ReactGA.send('pageview');
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
